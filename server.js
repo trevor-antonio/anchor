0e0184f7b794afd88adcb6d93577c09fa80d8070
Trevor Antonio <codertrev@proton.me>
2026-09-03 16:56:23 -0700

    Finished builidng OIDC. Built out app.post webathn/register API call

--- server.js
+++ server.js
@@ -38,3 +38,48 @@ app.use(
         saveUninitialized: false
  })
 )
+//
+// NEED TO ADD THESE TO .env
+//
+const oidcConfig = {
+    //don't req auth for every req
+    authRequired: false,
+    //if /login route is used,logout by redirecting to OIDC provider's own logout endpoint
+    auth0Logout: true,
+    //secret used to encrypt OIDC session cookie
+    secret: process.env.OIDC_SECRET,
+    baseURL: process.env.BASE_URL,
+    //client ID (Google/Mircosoft/AUTH0) issues
+    clientID: process.env.OIDC_CLIENT_ID,
+    //tells the lib where to find OIDC metadata
+    issuerBaseURL: process.env.OIDC_ISSUER_URL
+}
+//regusters middleware; uses OIDC middleware
+app.use(auth(oidcConfig))
+
+//WEBAUTHN - REGISTRATION (creating a new passkey)
+
+// Express Core
+app.post('/webauthn/register/options', async (req, res) => {
+    try {
+        const user = req.session.user
+
+        if(!user) {
+            return res.status(401).json({message: 'Authentication required'})
+        }
+        const options = await generateRegistrationOptions({
+            rpName: 'Anchor',
+            rpID: process.env.WEBAUTHN_RP_ID,
+            userName: user.email,
+            excludeCredentials: user.credentials || []
+        })
+
+        //save challange in session so /verify can confirm it matches later
+        req.session.currentChallange
+
+        res.json({ success: true })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Unable to verify passkey registration'})
+    }
+})
