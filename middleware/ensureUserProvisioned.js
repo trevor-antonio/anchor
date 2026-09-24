30e91231a6e49876b445991991a05a33981dca19
Trevor Antonio <codertrev@proton.me>
2026-09-24 16:16:41 -0700

    Completed building out ensureUserProvisioned middleware

--- middleware/ensureUserProvisioned.js
+++ middleware/ensureUserProvisioned.js
@@ -3,22 +3,31 @@ const { pool } = require('../server')
 
 async function ensureUserProvisioned(req, res, next) {
     try {
-        // pulls the logged-in user's unique ID off the request
-        const auth0sub = req.oidc.user.sub
+        // pulls the logged-in user's unique ID (string) off the request
+        const auth0Sub = req.oidc.user.sub
 
         // Look for an existing user with this auth0_sub
-        const existingUser = await pool.query (
+        const existingUser = await pool.query(
             'SELECT * FROM users WHERE auth0_sub = $1',
             [auth0Sub]
         )
         if (existingUser.rows.length > 0) {
             //found them - attach and move on
             req.dbUser = existingUser.rows[0]
-            return next
-
+            return next()
         }
+        //First time logging in, create their row
 
-    } catch (err){
-        next (err)
+        const newUser = await pool.query(
+            'INSERT INTO users (auth0_sub, email) VALUES ($1, $2) RETURNING *',
+            [auth0Sub, req.oidc.user.email]
+        )
+        req.dbUser = newUser.rows[0]
+        next()
+
+    } catch (err) {
+        next(err)
+    }
 }
-} 
+
+module.exports = { ensureUserProvisioned }
